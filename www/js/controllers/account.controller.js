f050a231fd2964165aa43456bc504b3baab830ae
jesus.abarca <jabas06@gmail.com>
2015-06-21 11:06:02 -0500

    update(acconut): disable back button after logout

--- www/js/controllers/account.controller.js
+++ www/js/controllers/account.controller.js
@@ -1,11 +1,11 @@
 angular.module('starter.controllers')
-    .controller('AccountCtrl', function($scope, user, Auth, Ref, $firebaseObject) {
+    .controller('AccountCtrl', function($scope, $ionicHistory, user, Auth, Ref, $firebaseObject) {
         $scope.vm = {};
         var self = $scope.vm;
         var profile = $firebaseObject(Ref.child('users').child(user.uid));
 
         self.user = user;
-        self.logout = function() { Auth.$unauth(); };
+        self.logout = logout;
 
 
         init();
@@ -13,6 +13,15 @@ angular.module('starter.controllers')
         // *********************************
         // Internal
         // *********************************
+
+        function logout(){
+            $ionicHistory.nextViewOptions({
+                disableAnimate: true,
+                disableBack: true
+            });
+
+            Auth.$unauth();
+        }
 
         function init() {
             profile.$bindTo($scope, 'vm.profile');
