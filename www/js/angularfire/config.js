569efca52f6978ea6c7def8b3f4df013adc17fc6
jesus.abarca <jabas06@gmail.com>
2016-02-15 22:36:06 -0600

    progress

--- www/js/angularfire/config.js
+++ www/js/angularfire/config.js
@@ -1,3 +1,3 @@
 angular.module('firebase.config', [])
-  .constant('FBURL', 'https://gasolineras.firebaseio.com')
+  .constant('FBURL', 'https://gasvipdb.firebaseio.com')
   .constant('SIMPLE_LOGIN_PROVIDERS', ['anonymous','facebook']);
